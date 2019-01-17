c29fea134ca581adbe09441726a2d507be51f1d9
张超超 <zhangchaochao@homolo.com>
2019-01-17 10:24:22 +0800

    初始化项目

---
+++ zboot-web/src/main/java/org/zboot/web/ZbootApplication.java
@@ -0,0 +1,14 @@
+package org.zboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbootApplication.class, args);
+	}
+
+}
+
